483aa2ebc5ecc5ac92943540b986b6566fa80d21
jeremy-nevado <jeremynevado@gmail.com>
2020-09-22 19:14:56 -0400

    Updates

---
+++ precache-manifest.b20dc85c34f80283d24880c35dab2308.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52d518e21ac65deadb6188e8ba49394",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "63a50e4b2e658fcc4a9e",
+    "url": "/monsters-rolodex/static/css/main.51467569.chunk.css"
+  },
+  {
+    "revision": "57595fc6caf45f548129",
+    "url": "/monsters-rolodex/static/js/2.c272e29f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.c272e29f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63a50e4b2e658fcc4a9e",
+    "url": "/monsters-rolodex/static/js/main.630d029b.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
